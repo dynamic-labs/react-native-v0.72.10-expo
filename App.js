c38f67130d65d1c1a6720be3f5b29f9032589499
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2024-07-31 11:17:13 -0300

    adds dynamic sdk on react-native 0.72.10

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { createClient } from "@dynamic-labs/client";
+import { useReactiveClient } from "@dynamic-labs/react-hooks";
+import { ReactNativeExtension } from "@dynamic-labs/react-native-extension";
+
+const dynamic = createClient({
+  environmentId: "--- env id ---",
+}).extend(ReactNativeExtension());
+
+const useDynamic = () => useReactiveClient(dynamic);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <dynamic.reactNative.WebView />
+
+      <View style={styles.container}>
+        <Text>Open up App to start working on your app!</Text>
+        <StatusBar style="auto" />
+
+        <Button title="open auth" onPress={() => dynamic.ui.auth.show()} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
